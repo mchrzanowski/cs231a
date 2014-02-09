39bced28c104c3a06573a3db3b186debf1da1d7c
Francois Chaubard <fchaubard@gmail.com>
2014-02-08 16:56:50 -0800

    added fv and feature augmentation

---
+++ src/feature_augmentation.py
@@ -0,0 +1,12 @@
+# feature augmentation takes in a post PCA's matrix and tacks on some meta information about each column such
+# as: coordinates, colors, illumination, etc ... the columns of B are whitened sift descriptors
+
+def feature_augmentation(B, locations, coordinates=True,colors=False,Illumination=False):
+
+	if (type(B).__module__!='numpy'):
+                print "Not a valid numpy matrix..."
+                return
+        else:
+		#for x in range(0, B.shape[1]):
+		return B	
+
