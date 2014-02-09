07fd5cd24184114e52cf1c2d36113ea47fb00a27
Francois Chaubard <fchaubard@gmail.com>
2014-02-08 18:13:16 -0800

    fixed aug

--- src/feature_augmentation.py
+++ src/feature_augmentation.py
@@ -11,11 +11,12 @@ def feature_augmentation(B, locations, coordinates=True,colors=False,Illuminatio
         x_vect=[]
         y_vect=[]
         
-        for i in xrange(0, B.shape[1]):
+        for i in xrange(0, B.shape[0]):
             x_vect.append(locations[i].pt[0])
             y_vect.append(locations[i].pt[1])
-
-        B=numpy.concatenate((B, x_vect), axis=0)
-        B=numpy.concatenate((B, y_vect), axis=0)
+        print len(x_vect)
+        print B.shape
+        B=numpy.concatenate((B,numpy.array([numpy.asarray(x_vect)]).T),axis=1)
+        B=numpy.concatenate((B,numpy.array([numpy.asarray(y_vect)]).T),axis=1)
         return B	
 
