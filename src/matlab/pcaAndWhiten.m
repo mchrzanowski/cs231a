b593f5208cc9498441f557e30d827a54e1e49464
Mike Chrzanowski <mike.chrzanowski0@gmail.com>
2014-02-09 02:49:07 -0800

    capital letters :)

--- src/matlab/pcaAndWhiten.m
+++ src/matlab/pcaAndWhiten.m
@@ -5,8 +5,8 @@ function X = pcaAndWhiten(X)
     epsilon = 1e-4;
     X = X - repmat(mean(X, 1), size(X, 1), 1);
     covar = X * X' / size(X, 2);
-    [u, s, ~] = svd(covar, 'econ');
-    s = s(1:64, 1:64);
-    u = u(:, 1:64);
-    X = diag(1 ./ sqrt(diag(s) + epsilon)) * u' * X;
+    [U, S, ~] = svd(covar, 'econ');
+    S = S(1:64, 1:64);
+    U = U(:, 1:64);
+    X = diag(1 ./ sqrt(diag(S) + epsilon)) * U' * X;
 end
