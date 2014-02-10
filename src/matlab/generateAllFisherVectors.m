ad83181e220272677f5ad25f64b5fdb19c85311d
Mike Chrzanowski <mike.chrzanowski0@gmail.com>
2014-02-10 13:31:25 -0800

    subgradient descent learning

--- src/matlab/generateAllFisherVectors.m
+++ src/matlab/generateAllFisherVectors.m
@@ -16,8 +16,13 @@ function generateAllFisherVectors
                 continue;
             end
             f_q_img_path = strcat(f_q_subfolder, image.name);
+            f_q_output_path = strcat(output_dir, image.name);
+            if exist(f_q_output_path, 'file') ~= 0
+                continue;
+            end
             fv = generateFisherVector(f_q_img_path);
-            dlmwrite(strcat(output_dir, image.name), fv);
+            fv = fv / norm(fv, 2);
+            dlmwrite(f_q_output_path, fv);
         end
     end
 end
---
+++ src/python/optimization.py
@@ -0,0 +1,30 @@
+import random
+from numpy.linalg import norm
+from numpy import dot
+
+def subgradient_optimization(W, fvs, person_to_indices, eta=0.01, iterations=1000000):
+    b = random.rand() * fvs.shape(1)
+    for i in xrange(1, iterations + 1):
+        if random.rand() < 0.5
+            while True:
+                person = random.choice(person_to_indices)
+                if len(person_to_indices[person]) == 1:
+                    continue
+                i, j = random.sample(person_to_indices[person], 2)
+                y = -1
+                break
+        else:
+            first_person, second_person = random.sample(person_to_indices, 2)
+            i = random.choice(person_to_indices[first_person])
+            j = random.choice(person_to_indices[second_person])
+            y = +1
+
+        fv_diff = fvs(:, i) - fvs(:, j)
+        dist = norm(dot(W, fv_diff), 2) ** 2
+
+        # update W & b
+        if y * (b - dist) < 1:
+            W -= eta * y * dot(dot(W, fv_diff), fv_diff.T)
+            b += (1 + dist - b) / i     # iterative mean update.
+
+    return W
