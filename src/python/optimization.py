ea1cfc5f78049243dc10ff435251b3b0b83b06af
Mike Chrzanowski <mike.chrzanowski0@gmail.com>
2014-02-10 23:31:39 -0800

    changes

--- src/python/optimization.py
+++ src/python/optimization.py
@@ -1,30 +1,32 @@
 import random
 from numpy.linalg import norm
-from numpy import dot
+from numpy import dot, outer
 
 def subgradient_optimization(W, fvs, person_to_indices, eta=0.01, iterations=1000000):
     b = 0
     for i in xrange(iterations):
-        if random.rand() < 0.5
+        if i % 1000 == 0: print i
+        if random.random() < 0.5:
             while True:
-                person = random.choice(person_to_indices)
+                person = random.choice(person_to_indices.keys())
                 if len(person_to_indices[person]) == 1:
                     continue
                 i, j = random.sample(person_to_indices[person], 2)
-                y = -1
+                y = +1
                 break
         else:
             first_person, second_person = random.sample(person_to_indices, 2)
             i = random.choice(person_to_indices[first_person])
             j = random.choice(person_to_indices[second_person])
-            y = +1
+            y = -1
 
-        fv_diff = fvs(:, i) - fvs(:, j)
-        dist = norm(dot(W, fv_diff), 2) ** 2
+        fv_diff = fvs[:, i] - fvs[:, j]
+        first_op = dot(W, fv_diff)
+        dist = norm(first_op, 2) ** 2
 
         # update W & b
         if y * (b - dist) < 1:
-            W -= eta * y * dot(dot(W, fv_diff), fv_diff.T)
+            W -= eta * y * outer(first_op, fv_diff)
             b += (y + dist - b) / (i + 1)     # iterative mean update.
 
     return W
