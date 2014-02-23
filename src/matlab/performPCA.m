d76fb8aefe1bacac2b698f3ba4d8578d236684dc
Mike Chrzanowski <mike.chrzanowski0@gmail.com>
2014-02-23 13:28:24 -0800

    Update performPCA.m

--- src/matlab/performPCA.m
+++ src/matlab/performPCA.m
@@ -1,7 +1,6 @@
 function X = performPCA(X)
 % perform PCA
 % assume X is a 128xD matrix
-% create a whitened 64xD matrix
     X = X - repmat(mean(X, 1), size(X, 1), 1);
     covar = X * X' / size(X, 2);
     [U, ~, ~] = svd(covar, 'econ');
