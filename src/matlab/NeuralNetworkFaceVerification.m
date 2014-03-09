3449767098c731796a0272ba2b50a55b84de894f
Francois Chaubard <fchaubar@napoli11.Stanford.EDU>
2014-03-08 23:45:41 -0800

    added svm

--- src/matlab/NeuralNetworkFaceVerification.m
+++ src/matlab/NeuralNetworkFaceVerification.m
@@ -1,13 +1,12 @@
 %Train and test the neural network procedure
 clear all; close all; clc;
-dbstop if error;
 
 curr_dir = pwd;
 cd('~/vlfeat-0.9.18/toolbox/')
 vl_setup
 cd(curr_dir)
 
-num_iters=10^2;
+num_iters=10^6;
 
 image_dir = '~/lfw-deepfunneled/';
 U=dlmread('../../params/gmm_params_rooted_df/U_matrix');
@@ -15,16 +14,27 @@ M=dlmread('../../params/gmm_params_rooted_df/means');
 D=dlmread('../../params/gmm_params_rooted_df/diags');
 P=dlmread('../../params/gmm_params_rooted_df/priors');
 
-% generate all the fv's
-[fvs labels] = createTrainingFVs(image_dir, U, M, D, P);
-save('fvs_francois.mat','fvs');
-save('labels_francois.mat','labels');
+display('checking for fvs')
+fv_file = sprintf('%s/fvs_francois.mat',pwd());
+
+if exist(fv_file, 'file') ~= 2
+	% generate all the fv's
+	display('fvs didnt exist! Creating them..')
+	[fvs labels] = createTrainingFVs(image_dir, U, M, D, P);
+	save('fvs_francois.mat','fvs');
+	save('labels_francois.mat','labels');
+else
+	display('fvs did exist! Loading them..')
+	load('fvs_francois.mat');
+	load('labels_francois.mat');
+
+end
 
 unique_people = unique(labels);
 histogram = histc(labels,1:labels(end));
 moreThan1Picture = find(histogram>1);
 
-fv_diff=zeros(67584,num_iters);
+fv_diff=[];
 
 for i=1:num_iters
     
@@ -43,9 +53,12 @@ for i=1:num_iters
         fv1 = fvs(:,choices(1));
         fv2 = fvs(:,choices(2));
     end
-    fv_diff(:,i)=fv1-fv2;
+    fv_diff=[fv_diff sparse(fv1-fv2)];
     
     
     
     
 end
+
+
+
