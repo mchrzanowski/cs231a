a7eb9a5c61597b8b414e56633a307721dfa4842c
Mike Chrzanowski <mike.chrzanowski0@gmail.com>
2014-03-11 21:30:29 -0700

    changes

--- src/matlab/generateSIFTDescriptors.m
+++ src/matlab/generateSIFTDescriptors.m
@@ -12,6 +12,12 @@ function [descriptors keypts] = generateSIFTDescriptors(img_file)
         min_y = min(bboxes(:, 2));
         max_x = max(bboxes(:, 1) + bboxes(:, 3));
         max_y = max(bboxes(:, 2) + bboxes(:, 4));
+
+        min_x = max(1, min_x);
+        min_y = max(1, min_y);
+        max_x = min(size(img, 1), max_x);
+        max_y = min(size(img, 2), max_y);
+        
         if max_x > min_x && max_y > min_y
             img = img(min_x:max_x, min_y:max_y, :);
         end
