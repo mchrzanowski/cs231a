3e9ec2447fc0454fa756aa60c25040b4875b10d3
Mike Chrzanowski <mike.chrzanowski0@gmail.com>
2014-04-01 01:31:56 -0700

    Update generateSIFTDescriptors.m

--- src/matlab/generateSIFTDescriptors.m
+++ src/matlab/generateSIFTDescriptors.m
@@ -1,4 +1,4 @@
-function [descriptors other_features] = generateSIFTDescriptors(img_file)
+function [descriptors keypts] = generateSIFTDescriptors(img_file)
     img_height = 160;
     img_width = 125;
 
@@ -12,13 +12,11 @@ function [descriptors other_features] = generateSIFTDescriptors(img_file)
     img = imresize(img, [img_height, img_width]);   % (numrows, numcols)
     skin = imresize(skin, [img_height, img_width]);
     
-    [orig_keypts descriptors] = generateDenseSIFTDescriptors(img);
+    [keypts descriptors] = generateDenseSIFTDescriptors(img);
     %descriptors = convertToRootSIFT(descriptors);
     
     % add spatial information.
-    keypts = orig_keypts ./ repmat([img_width; img_height], 1, size(descriptors, 2));
+    keypts = keypts ./ repmat([img_width; img_height], 1, size(descriptors, 2));
     keypts = keypts - 0.5;
-
-    other_features = [keypts; skin_per_keypt];
 
 end
